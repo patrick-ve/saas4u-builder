a00f6ad6a789e9c28a41b9acee503a89d6d1ad4a
Patrick van Everdingen <patrick@passionatepeople.io>
2025-05-12 22:04:12 +0200

    chore: update builder devtools config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-import BuilderDevTools from "@builder.io/dev-tools/next";
-import { withHydrationOverlay } from "@builder.io/react-hydration-overlay/next";
+import BuilderDevTools from '@builder.io/dev-tools/next';
+import { withHydrationOverlay } from '@builder.io/react-hydration-overlay/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -33,18 +33,20 @@ const nextConfig = {
     ],
   },
   experimental: {
-    serverComponentsExternalPackages: ["isolated-vm"],
+    serverComponentsExternalPackages: ['isolated-vm'],
   },
 };
 // export default nextConfig;
 const configWithOverlay = BuilderDevTools()(
-  withHydrationOverlay({
-    /**
-     * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
-     * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
-     */
-    appRootSelector: "main",
-  })(nextConfig)
+  BuilderDevTools()(
+    withHydrationOverlay({
+      /**
+       * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+       * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+       */
+      appRootSelector: 'main',
+    })(nextConfig)
+  )
 );
 
 export default configWithOverlay;
