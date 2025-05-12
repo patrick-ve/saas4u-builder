30502a3614f0ecd055ddcd09d26254ece20c624f
Patrick van Everdingen <patrick@passionatepeople.io>
2025-05-12 11:32:34 +0200

    feat: include locale picker component

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,23 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
-import * as React from "react"
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { cn } from "@/lib/utils"
+'use client';
+import * as React from 'react';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { AuthSlider } from "./AuthSlider"
-import { CartSlider } from "./CartSlider"
-import { SideNav } from "./SideNav"
+} from '@/components/ui/navigation-menu';
+import { SideNav } from './SideNav';
 import { BuilderContent } from '@builder.io/react';
-
+import LocalePicker from '../Locale/Picker';
 
 export function Header({ headerContent }: any) {
   return (
@@ -25,7 +20,6 @@ export function Header({ headerContent }: any) {
       {(data) => (
         <header className="w-full flex flex-1 border-y mb-4">
           <div className="px-4 p-3 flex justify-between container">
-
             <NavigationMenuItem className="flex md:hidden">
               <SideNav />
             </NavigationMenuItem>
@@ -43,32 +37,48 @@ export function Header({ headerContent }: any) {
               <NavigationMenuList className="justify-around w-full">
                 {data?.headerLinks.map((item: any, index: number) => {
                   return (
-                    <Button key={index} variant="link" className="text-md">
-                      <Link href={item.path || '/'} legacyBehavior passHref >
+                    <Button
+                      key={index}
+                      variant="link"
+                      className="text-md"
+                    >
+                      <Link
+                        href={item.path || '/'}
+                        legacyBehavior
+                        passHref
+                      >
                         {/* <NavigationMenuLink className={navigationMenuTriggerStyle()}> */}
-                        <span className={`uppercase ${item.highlight ? "text-rose-500" : ""}`}>{item.label}</span>
+                        <span
+                          className={`uppercase ${item.highlight ? 'text-rose-500' : ''}`}
+                        >
+                          {item.label}
+                        </span>
                         {/* </NavigationMenuLink> */}
                       </Link>
                     </Button>
-                  )
+                  );
                 })}
               </NavigationMenuList>
             </NavigationMenu>
-            <div className="flex items-center">
-              <CartSlider variant="black" />
-              <AuthSlider variant="black" />
+            <div className="flex items-center gap-3">
+              <LocalePicker
+                availableLocales={[
+                  { code: 'en-US', label: '🇺🇸 English' },
+                  { code: 'de-DE', label: '🇩🇪 Deutsch' },
+                  { code: 'fr-FR', label: '🇫🇷 Français' },
+                ]}
+              />
             </div>
           </div>
         </header>
-      )
-      }
+      )}
     </BuilderContent>
   );
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
@@ -76,18 +86,20 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
           {...props}
         >
-          <div className="text-sm font-light leading-none">{title}</div>
+          <div className="text-sm font-light leading-none">
+            {title}
+          </div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
         </a>
       </NavigationMenuLink>
     </li>
-  )
-})
-ListItem.displayName = "ListItem"
+  );
+});
+ListItem.displayName = 'ListItem';
